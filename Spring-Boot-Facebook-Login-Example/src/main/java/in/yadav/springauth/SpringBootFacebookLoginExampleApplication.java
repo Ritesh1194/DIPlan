064c268f36d2397ba016e8f53913a0896e7a4824
Ritesh1194 <yadavritesh1194@gmail.com>
2020-04-02 11:57:10 +0530

    [Ritesh Yadav].add Spring-Boot-Facebook-Login-Example Completed Using Spring Auth 2.0

---
+++ Spring-Boot-Facebook-Login-Example/src/main/java/in/yadav/springauth/SpringBootFacebookLoginExampleApplication.java
@@ -0,0 +1,12 @@
+package in.yadav.springauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFacebookLoginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFacebookLoginExampleApplication.class, args);
+	}
+}
---
+++ Spring-Boot-Facebook-Login-Example/src/test/java/in/yadav/springauth/SpringBootFacebookLoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.yadav.springauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFacebookLoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
